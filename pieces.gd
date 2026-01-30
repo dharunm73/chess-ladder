15db64c24d62adbee4cd50a93d58bd0e4f245dbe
Dharun <dharunm7733@gmail.com>
2026-01-30 18:59:59 +0530

    Separating scenes

--- pieces.gd
+++ pieces.gd
@@ -1,12 +1,13 @@
-extends Node
+extends CharacterBody2D
 
 func _ready() -> void:
-	pass
+	input_pickable = true
+	input_event.connect(_on_character_input)
 	
-func _input(event):
+func _on_character_input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == event and  and event.pressed:
-			
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("clicked")
 	
 func _process(_delta: float) -> void:
 	pass
